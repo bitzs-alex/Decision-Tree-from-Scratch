1bc2004c8585324bf7f9f718d4a4d5a90f362cb6
bitzs-alex <bitzsalex@gmail.com>
2022-10-08 22:18:23 +0900

    feat: Completed Stage Two
    I completed stage two of the project.

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,43 +1,133 @@
 # write your code here
 import numpy as np
+import pandas as pd
 
 
-def gini(array: list):
-    """Calculate the gini impurity for a given array
+class GiniImpurity:
+    """Gini Impurity Calculator"""
 
-    Args:
-        array: list
+    def __init__(self, df: pd.DataFrame, cat_variable: str = "Survived"):
+        """Constructor of the class
 
-    Returns:
-        float: the gini impurity value
-    """
-    array = np.array(array).astype(int)
-    probability_1 = array.sum() / array.size
-    probability_0 = 1 - probability_1
+        Args:
+            df (pd.DataFrame): a dataframe to calculate the Gini Impurity Value
+            cat_variable (str): a categorical variable to refer to when calculating the Gini Impurity
+        """
+        self.__df = df.copy()
+        self.__cat_variable = cat_variable
 
-    return 1 - (np.square(probability_0) + np.square(probability_1))
+    def discard(self, col_name: str):
+        """Discarding or removing a column from the dataframe
+
+        Args:
+            col_name (str): a column name to remove from the dataframe
+        """
+        if col_name in self.__df.columns:
+            self.__df.drop(columns=col_name, inplace=True)
+
+    def gini_index(self, col_name: str, value: any):
+        """Calculate the gini index of each categorical values
+
+        Args:
+            col_name (str): a column name to calculate the individual gini index
+            value (any): a value found in the column to calculate the individual gini index
+
+        Returns:
+            Calculated gini index of a given value from specified column
+        """
+        df_filtered = self.__df[self.__df[col_name] == value]  # filtering the dataframe
+        value_counts = df_filtered[self.__cat_variable].value_counts()  # calculating the categorical values count
+
+        return 1 - sum(
+            np.square(value / df_filtered.shape[0]) for value in value_counts
+        )
+
+    def weighted_gini_index(self, col_name: str):
+        """Calculates the weighted gini index of a column
+
+        Args:
+            col_name (str): a column name to calculate the weighted gini index
+
+        Returns:
+            calculated weighted gini index of a given column
+        """
+        value_counts = self.__df[col_name].value_counts()  # counting the number of different values in a column
+        number_of_values = self.__df[col_name].count()  # getting the total number of elements in a column
+
+        return sum(
+            # a probability of a given value multiplied by its gini index value
+            (value_counts[value] / number_of_values) * self.gini_index(col_name, value) for value in value_counts.index
+        )
+
+    def calculate(self, discard_col: str = None):
+        """Loop over the columns of the dataframe and calculate the gini index
+
+        Args:
+            discard_col (str, optional): if there is a column that must be dropped before calculating the gini index
+
+        Returns:
+            a tuple of minimum weighted gini index value and splitting column if a dataframe is splittable
+            if not None
+        """
+        if discard_col:
+            self.discard(discard_col)
+
+        # getting the columns to discard the categorical variable
+        columns = self.__df.columns.to_list()
+        columns.remove(self.__cat_variable)
+
+        # if the dataframe is splittable
+        # calculate the gini index, if not return None
+        if len(columns):
+            weighted_gini_values = {
+                # calculate the weighted gini index for all columns
+                col_name: self.weighted_gini_index(col_name) for col_name in columns
+            }
+
+            # find the minimum
+            values = weighted_gini_values.values()
+            minimum_gini = min(values)
+            next_splitter = list(weighted_gini_values.keys())[list(values).index(minimum_gini)]
+
+            return np.round(minimum_gini, 4), next_splitter
+
+    def split(self, discard_col: str = None):
+        """Split the dataframe after calculating the gini index
+
+        Args:
+            discard_col (str, optional): if there is a column that must be dropped before calculating the gini index
+
+        Returns:
+            a tuple of minimum weighted gini index value, splitting column name, and splits if a dataframe is splittable
+            if not None
+        """
+        if calculated_gini := self.calculate(discard_col):
+            gini_index, col_name = calculated_gini
+            value_counts = self.__df[col_name].value_counts()
+            chosen_value = value_counts.idxmax()
+
+            # splitting the dataset
+            splits = [
+                list(self.__df[self.__df[col_name] == value].index)
+                for value in value_counts.index
+            ]
+
+            # discarding the column after splitting
+            self.discard(col_name)
+
+            return gini_index, col_name, chosen_value, *splits
 
 
-def weighted_gini(split_one: list, split_two: list):
-    """Calculate the weighted gini impurity
+def runner():
+    path_to_dataset = input()
+    df = pd.read_csv(path_to_dataset, index_col=0)
 
-    Args:
-        split_one(list): a split from a given dataset
-        split_two(list): another split from a given dataset
+    gini_impurity = GiniImpurity(df)
+    split_result = gini_impurity.split()
 
-    Returns:
-        float: the value of weighted gini impurity value
-    """
-    first_size = len(split_one)
-    second_size = len(split_two)
-    total_size = first_size + second_size
-
-    return (first_size / total_size * gini(split_one)) \
-                    + (second_size / total_size * gini(split_two))
+    if split_result:
+        print(*split_result)
 
 
-array = input().split()
-split_one = input().split()
-split_two = input().split()
-
-print(np.round(gini(array), 2), np.round(weighted_gini(split_one, split_two), 2))
+if __name__ == "__main__":
+    runner()
--- Decision Tree from Scratch/task/tests.py
+++ Decision Tree from Scratch/task/tests.py
@@ -1,10 +1,10 @@
 from hstest import *
 
 
-ANSWER = [0.0, 'Sex', 0, [0, 4, 5, 6, 7], [1, 2, 3, 8, 9]]
+ANSWER = ["Made split: Sex is 0", "Made split: Pclass is 2", "Made split: Pclass is 3"]
 
 
-class SplitTest(StageTest):
+class RecSplitTest(StageTest):
 
     @dynamic_test
     def test(self):
@@ -12,44 +12,23 @@ class SplitTest(StageTest):
         pr.start()
         if not pr.is_waiting_input():
             raise WrongAnswer("You program should input the path to the file")
-        output = pr.execute("test/data_stage2.csv").strip()
-        output = output.replace(",", "").replace("]", "[").split("[")
-        output = [x for x in output if x != "" and x != " "]
-        if len(output) != 3:
-            raise WrongAnswer("Your answer should contain five items: float value, string, value and two lists.")
-        res = []
-        first = output[0].strip().split()
-        try:
-            res.append(float(first[0]))
-        except Exception:
-            raise WrongAnswer("The first value should be float.")
-        res.append(first[1])
-        try:
-            res.append(int(first[2]))
-        except Exception:
-            raise WrongAnswer("The third value should be integer.")
-        try:
-            res.append([int(x) for x in output[1].split()])
-        except Exception:
-            raise WrongAnswer("The fourth item should be a list of integers.")
-        try:
-            res.append([int(x) for x in output[2].split()])
-        except Exception:
-            raise WrongAnswer("The fifth item should be a list of integers.")
-        if len(res) != 5:
-            raise WrongAnswer("Your answer should contain five items: float value, string, value, two lists.")
+        output = pr.execute("test/data_stage3.csv").strip()
+        res = output.split("\n")
+        res = [x.strip() for x in res]
+        if len(res) != 3:
+            raise WrongAnswer("Wrong number of splits. Your function should make a split three times (on given data)."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
         if res[0] != ANSWER[0]:
-            raise WrongAnswer("Wrong Gini score (the first item in your answer).")
+            raise WrongAnswer("Wrong first log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
         if res[1] != ANSWER[1]:
-            raise WrongAnswer("Wrong threshold feature (the second item in your answer).")
+            raise WrongAnswer("Wrong second log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
         if res[2] != ANSWER[2]:
-            raise WrongAnswer("Wrong threshold value (the third item in your answer).")
-        if res[3] != ANSWER[3]:
-            raise WrongAnswer("Wrong list of left node indexes (the fourth item in your answer).")
-        if res[4] != ANSWER[4]:
-            raise WrongAnswer("Wrong list of right node indexes (the fifth item in your answer).")
+            raise WrongAnswer("Wrong third log message. Correct message template: 'Made split: Sex is 1'."
+                              "\nIt may be useful to check the traversal order in the recursive split function: go to the left child first.")
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    SplitTest().run_tests()
+    RecSplitTest().run_tests()
